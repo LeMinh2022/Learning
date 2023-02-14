26393762974c5ac30139aba2d689e20ac23da9c7
Le Minh <quanlycanhan2020@gmail.com>
2023-02-14 15:29:10 +0700

    query parameters and form default behavior

--- NodeJS/src/index.js
+++ NodeJS/src/index.js
@@ -5,27 +5,36 @@ const app = express();
 const path = require('path');
 const { extname } = require('path');
 const port = 3000;
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
+
 // template engien 
-app.engine('hbs', hbs.engine(
-  {extname: '.hbs'}
-));
+app.engine('hbs', hbs.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'))
 
-// 
+// static link 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.render('home')
-  
-})
+  res.render('home');
+});
 
-app.get('/test', (req, res) => {
+app.get('/search', (req, res) => {
+  console.log(req.query)
+  res.render('search');
+});
+
+app.get('/news', (req, res) => {
+  console.log(req.query.q)
   res.render('news');
 });
 
+app.post('/search', (req, res) => {
+  console.log(req.query)
+  res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
