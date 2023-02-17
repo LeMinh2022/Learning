142ef6427737a3cdfb9f6fbb0bf3a5896d342842
Le Minh <quanlycanhan2020@gmail.com>
2023-02-17 18:31:04 +0700

    C# first hello and installc# core

---
+++ C#/CS01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CS01{
+    class Program{
+        // ham main de chay chuong trinh
+        static void Main (string[] args){
+            // day la noi dung in ra man hinh
+            Console.WriteLine("Xin chao c#");
+            // dong ghi chu thu 2
+            System.Console.WriteLine("ok anh em");
+
+            Tong(2,3);
+        }
+
+        /// <summary>
+        /// Phuong thuc tra ve tong 2 so nguyen
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+
+        static int Tong (int a, int b){
+            return (a+b);
+        }
+    }
+
+    class Abc 
+    {
+        static void XinChao(){
+            
+        }
+        
+    }
+}
+
+namespace MynameSpace{
+
+    class Abc{
+
+    }
+}
