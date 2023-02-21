6e8c5ae3e848a0489bf86d01e5e8f7aabb2edc10
Le Minh <quanlycanhan2020@gmail.com>
2023-02-21 09:12:02 +0700

    phep toan so sanh

---
+++ C#/CS04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CS04{
+
+    class Program{
+
+        static void Main(string[] args){
+            bool isOnline;
+            isOnline = false;
+            Console.WriteLine(isOnline);
+
+            //phep toan so sanh
+            // == > < >= <= !=
+            int a =6;
+            int b= 7;
+            bool kq;
+
+            kq = a ==b;
+            Console.WriteLine(" a == b --> {0}",kq);
+            Console.WriteLine($" a != b --> {a!=b}",kq);
+
+            // && || !
+
+            bool x = false;
+            bool y = true;
+
+            Console.WriteLine(x || y);
+            Console.WriteLine(x && y);
+
+        }
+    }
+}
+
